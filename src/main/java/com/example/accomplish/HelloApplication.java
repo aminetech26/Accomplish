2a79411b534208a8fc270385e674beb4ceba01da
ilyesblidi <ll_blidi@esi.dz>
2023-05-18 15:21:38 +0100

    Adding (Sign_In & Sign_Up) pages "Also Navigation between them"

--- src/main/java/com/example/accomplish/HelloApplication.java
+++ src/main/java/com/example/accomplish/HelloApplication.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
@@ -12,13 +15,16 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setWidth(1200);
+        stage.setHeight(625);
+        stage.setTitle("Accomplish");
         stage.setScene(scene);
         stage.show();
+
     }
-    /*
+
     public static void main(String[] args) {
         launch();
     }
-    */
+
 }
--- src/main/java/com/example/accomplish/HelloController.java
+++ src/main/java/com/example/accomplish/HelloController.java
@@ -1,14 +1,58 @@
 package com.example.accomplish;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.EventObject;
+import java.util.Objects;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    public Label Sing_In;
+    public Label Sing_Up;
+    private Stage stage;
+    private Parent root;
+    private EventObject event;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void handleLabel1Click(MouseEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Sign-Up_Page.fxml")));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void handleLabel2Click(MouseEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void GoToFeedPage(MouseEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Feed_Page.fxml")));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
+
